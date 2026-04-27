a821bde33065cec206718c38216f79c4a5619869
yama-ctf <6043_2024_06373@g.miyazaki-c.ed.jp>
2026-04-27 15:05:59 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let correctAnswer = "flag{test}";
+
+function checkAnswer() {
+  let userAnswer = document.getElementById("answer").value;
+  let result = document.getElementById("result");
+
+  if (userAnswer === correctAnswer) {
+    result.textContent = "正解！";
+  } else {
+    result.textContent = "不正解";
+  }
+}
