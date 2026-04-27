946800ae95c5494cbab5078c935db007c3924554
yama-ctf <6043_2024_06373@g.miyazaki-c.ed.jp>
2026-04-27 15:28:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 let correctAnswer = "flag{test}";
 
-function checkAnswer() {
-  let userAnswer = document.getElementById("answer").value;
-  let result = document.getElementById("result");
 
-  if (userAnswer === correctAnswer) {
-    result.textContent = "正解！";
-  } else {
-    result.textContent = "不正解";
-  }
-}
 //テスト
 function checkAnswer() {
   const userAnswer = document.getElementById("answer").value.trim().toLowerCase();
