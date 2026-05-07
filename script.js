8f8ffee5723ad2357f7fb90a17b14011b26195ba
yama-ctf <6043_2024_06373@g.miyazaki-c.ed.jp>
2026-05-07 15:30:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-let correctAnswer = "flag{test}";
+let questions = [];
+let currentQuestion = 0;
 
+// JSON読み込み
+fetch("questions.json")
+  .then(response => response.json())
+  .then(data => {
+    questions = data;
+    showQuestion();
+  });
+
+// 問題表示関数
+function showQuestion() {
+  document.getElementById("question").textContent =
+    questions[currentQuestion].question;
+}
+
+// 正解判定
 function checkAnswer() {
-  let userAnswer = document.getElementById("answer").value;
-  let result = document.getElementById("result");
+
+  let userAnswer =
+    document.getElementById("answer").value;
+
+  let correctAnswer =
+    questions[currentQuestion].answer;
+
+  let result =
+    document.getElementById("result");
 
   if (userAnswer === correctAnswer) {
     result.textContent = "正解！";
@@ -10,4 +33,3 @@ function checkAnswer() {
     result.textContent = "不正解";
   }
 }
-
